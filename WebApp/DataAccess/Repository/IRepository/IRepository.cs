e6ef7b41456eac36c4527c874c701383f8343ee7
ashyas <93943739+Ashyas@users.noreply.github.com>
2022-06-19 11:58:30 +0300

    update

---
+++ WebApp/DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string? includeProperties=null);
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+    }
+}
---
+++ WebApp/DataAccess/Repository/ShoppingCartRepsitory.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class ShoppingCartRepsitory : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ShoppingCartRepsitory(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public int DecrementCount(ShoppingCart shoppingCart, int count)
+        {
+            shoppingCart.Count -= count;
+            _db.SaveChanges();
+            return shoppingCart.Count;
+        }
+
+        public int IncrementCount(ShoppingCart shoppingCart, int count)
+        {
+            shoppingCart.Count += count;
+            _db.SaveChanges();
+            return shoppingCart.Count;
+        }
+    }
+}
